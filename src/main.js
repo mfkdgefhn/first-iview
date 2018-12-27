7a28ae318b9148976fab55383851e24c3a69ff44
mfkdgefhn <mfkdgefhn@163.com>
2018-12-27 11:19:24 +0800

    enter

---
+++ src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+
+// import { MenuItem, MenuGroup } from 'iview'
+// Vue.component('MenuGroup', MenuGroup)
+// Vue.component('MenuItem', MenuItem)
+
+Vue.use(iView)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
